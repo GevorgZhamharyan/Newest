b83c06322340be5eeccf59122b05c1669630c1cd
Gevorg Zhamharyan <gevorgzhamharyan@outlook.com>
2022-08-17 10:56:02 +0400

    Hey!

---
+++ Newest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Newest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 20;
+
+            Console.WriteLine(a + " " + b);
+
+            a = a + b;
+            b = a - b;
+            a = a - b; 
+
+            Console.WriteLine(a + " " + b); 
+        }
+    }
+}
